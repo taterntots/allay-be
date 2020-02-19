eae9f2a0388df54936de73bd2d6ff436f09c2664
spencer-mcguire <smcguire1826@gmail.com>
2020-02-19 14:37:48 -0600

    users and reviews helpers

--- helpers/reviews-model.js
+++ helpers/reviews-model.js
@@ -1,7 +1,33 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-  
-}
+  find,
+  findBy,
+  findById,
+  add
+};
 
 //Create some functions!
+
+function find() {
+  return db('reviews');
+}
+
+function findBy(filter) {
+  return db('reviews').where(filter);
+}
+
+function findById(id) {
+  return db('reviews')
+    .where({ id })
+    .first();
+}
+
+function add(review) {
+  return db('reviews')
+    .insert(review)
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
--- helpers/users-model.js
+++ helpers/users-model.js
@@ -1,7 +1,33 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-
-}
+  find,
+  findBy,
+  findById,
+  add
+};
 
 //Create some functions!
+
+function find() {
+  return db('users');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+function findById(id) {
+  return db('users as u')
+    .select('u.id', 'u.username')
+    .where({ id });
+}
+
+function add(user) {
+  return db('users')
+    .insert(user)
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
