6ff9344eb9502911a4d23b850edae8b4d641ebd4
spencer-mcguire <smcguire1826@gmail.com>
2020-02-19 16:44:36 -0600

    update reviews model

--- helpers/reviews-model.js
+++ helpers/reviews-model.js
@@ -4,13 +4,31 @@ module.exports = {
   findReviews,
   findReviewsBy,
   findReviewById,
-  addReview
+  addReview,
+  updateReview,
+  deleteReview
 };
 
 //Create some functions!
 
 function findReviews() {
-  return db('reviews');
+  return db('reviews as r')
+    .select(
+      'r.review_id',
+      'r.job_title',
+      'r.job_location',
+      'r.salary',
+      'r.interview',
+      'r.interview_rating',
+      'r.review',
+      'r.review_rating',
+      'r.user_id',
+      'u.username as reviewer',
+      'r.company_id',
+      'c.name as company_name'
+    )
+    .join('users as u', 'r.user_id', 'u.id')
+    .join('companies as c', 'r.company_id', 'c.id');
 }
 
 function findReviewsBy(filter) {
@@ -20,6 +38,22 @@ function findReviewsBy(filter) {
 function findReviewById(id) {
   return db('reviews')
     .where({ id })
+    .select(
+      'r.review_id',
+      'r.job_title',
+      'r.job_location',
+      'r.salary',
+      'r.interview',
+      'r.interview_rating',
+      'r.review',
+      'r.review_rating',
+      'r.user_id',
+      'u.username as reviewer',
+      'r.company_id',
+      'c.name as company_name'
+    )
+    .join('users as u', 'r.user_id', 'u.id')
+    .join('companies as c', 'r.company_id', 'c.id')
     .first();
 }
 
@@ -30,4 +64,17 @@ function addReview(review) {
       const [id] = ids;
       return findReviewById(id);
     });
+}
+
+function updateReview(id, changes) {
+  return db('reviews')
+    .where({ id })
+    .update(changes)
+    .then(count => (count > 0 ? findReviewById(id) : null));
+}
+
+function deleteReview(id) {
+  return db('reviews')
+    .where({ id })
+    .del();
 }
