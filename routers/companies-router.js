9bbef857999ae4a55251e2758ebd23d99fea62fa
wchamber01 <williamchambers@gmail.com>
2020-02-19 17:28:30 -0500

    built companies-router

--- routers/companies-router.js
+++ routers/companies-router.js
@@ -1,5 +1,55 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-//Create some routes!
+const Co = require("../helpers/company-model.js");
+
+/**************************************************************************/
+
+//                for endpoints beginning with /companies                   //
+
+/**************************************************************************/
+
+//***************** ADD NEW COMPANY *******************//
+router.post("/", (req, res) => {
+  let company = req.body;
+
+  Co.add(company)
+    .then(newCompany => {
+      res.status(201).json(newCompany);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "There was an error" });
+    });
+});
+
+//************** GET ALL COMPANIES ****************//
+router.get("/", (req, res) => {
+  Co.find()
+    .then(company => {
+      res.json(company);
+    })
+    .catch(err => res.send(err));
+});
+
+//************* UPDATE COMPANY INFO ****************//
+// router.put("/", (req, res) => {
+//   const changes = req.body;
+//   const id = req.company.id;
+
+//   Co.edit(id, changes)
+//     .then(info => {
+//       if (info) {
+//         res.status(200).json({ info: changes });
+//       } else {
+//         res.status(404).json({ message: "Error locating company info" });
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({ message: "Error updating company info" });
+//     });
+// });
+
+/**************************************************************************/
 
 module.exports = router;
