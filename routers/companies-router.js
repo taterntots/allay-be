6dcfcaeef583401852762138e513c4cebfd5baa7
wchamber01 <williamchambers@gmail.com>
2020-02-19 17:41:36 -0500

    built reviews-router

--- routers/companies-router.js
+++ routers/companies-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const Co = require("../helpers/company-model.js");
+const Co = require("../helpers/companies-model.js");
 
 /**************************************************************************/
 
--- routers/reviews-router.js
+++ routers/reviews-router.js
@@ -1,5 +1,41 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-//Create some routes!
+const Rev = require("../helpers/reviews-model.js");
+
+/**************************************************************************/
+
+//                for endpoints beginning with /companies                   //
+
+/**************************************************************************/
+
+//************** GET ALL REVIEWS ****************//
+router.get("/", (req, res) => {
+  Rev.find()
+    .then(review => {
+      res.json(review);
+    })
+    .catch(err => res.send(err));
+});
+
+//************* UPDATE REVIEW ****************//
+// router.put("/", (req, res) => {
+//   const changes = req.body;
+//   const id = req.review.id;
+
+//   Rev.edit(id, changes)
+//     .then(info => {
+//       if (info) {
+//         res.status(200).json({ info: changes });
+//       } else {
+//         res.status(404).json({ message: "Error locating review info" });
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({ message: "Error updating review info" });
+//     });
+// });
+
+/**************************************************************************/
 
 module.exports = router;
