880287a52af1432234e9712d233a9df1b6ba8d59
spencer-mcguire <smcguire1826@gmail.com>
2020-02-20 12:23:08 -0600

    user data structure

--- helpers/users-model.js
+++ helpers/users-model.js
@@ -1,55 +1,70 @@
-const db = require('../data/dbConfig.js');
-const mappers = require('./mappers');
+const db = require("../data/dbConfig.js");
+const mappers = require("./mappers");
 
 module.exports = {
-  findUsers,
-  findUsersBy,
-  findUserById,
-  findUserReviews,
-  addUser,
-  updateUser,
-  deleteUser
+	findUsers,
+	findUsersBy,
+	findUserById,
+	findUserReviews,
+	addUser,
+	updateUser,
+	deleteUser
 };
 
 //Create some functions!
 
 function findUsers() {
-  return db('users');
+	return db("users");
 }
 
 function findUsersBy(filter) {
-  return db('users').where(filter);
+	return db("users").where(filter);
 }
 
-function findUserById(id) {
-  return db('users as u')
-    .select('u.id', 'u.username')
-    .where({ id });
+// function findUserById(id) {
+// 	return db("users as u")
+// 		.select("u.id", "u.username")
+// 		.where({ id });
+// }
+
+function findUserById(userId) {
+	return db("reviews as r")
+		.where("r.user_id", userId)
+		.join("users as u", "r.user_id", "u.id")
+		.select("u.id", "u.username")
+		.then(result => {
+			// console.log(result[0].id);
+			return findUserReviews(result[0].id)
+				.then(reviewArray => {
+					// console.log(reviewArray)
+					return { ...result[0], reviews: reviewArray };
+				})
+				.catch(err => console.log(err));
+		})
+		.catch(err => console.log(err));
 }
 
 function findUserReviews(userId) {
-  return db('reviews')
-    .where('user_id', userId)
-    .then(reviews => reviews.map(review => mappers.reviewToBody(review)));
+	return db("reviews as r").where("r.user_id", userId);
 }
 
 function addUser(user) {
-  return db('users')
-    .insert(user)
-    .then(([id]) => {
-      return findUserById(id);
-    });
+	return db("users")
+		.insert(user)
+		.then(([id]) => {
+			return findUserById(id);
+		});
 }
 
 function updateUser(id, changes) {
-  return db('users')
-    .where({ id })
-    .update(changes)
-    .then(count => (count > 0 ? findUserById(id) : null));
+	return db("users")
+		.where({ id })
+		.update(changes)
+		.then(count => (count > 0 ? findUserById(id) : null));
 }
 
 function deleteUser(id) {
-  return db('users')
-    .where({ id })
-    .del();
+	return db("users")
+		.where({ id })
+		.del();
 }
--- routers/users-router.js
+++ routers/users-router.js
@@ -1,5 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Users = require("../helpers/users-model");
 
 //Create some routes!
+
+router.get("/user/:id", (req, res) => {
+	Users.findUserById(req.params.id)
+		.then(user => res.json(user))
+		.catch(err => console.log(err));
+});
+
+router.get("/user/reviews/:id", (req, res) => {
+	Users.findUserReviews(req.params.id)
+		.then(user => res.json(user))
+		.catch(err => console.log(err));
+});
 
 module.exports = router;
