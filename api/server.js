05e95f576c1f9ec296a07feb891f8c7f2ef66e74
wchamber01 <williamchambers@gmail.com>
2020-02-20 18:34:32 -0500

    added auth to routes @ server level

--- api/server.js
+++ api/server.js
@@ -26,8 +26,8 @@ server.get('/', (req, res) => {
 
 //routes with Auth applied
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-server.use('/api/organizations', companiesRouter);
-server.use('/api/reviews', reviewsRouter);
+server.use('/api/users', Auth, usersRouter);
+server.use('/api/organizations', Auth, companiesRouter);
+server.use('/api/reviews', Auth, reviewsRouter);
 
 module.exports = server;
