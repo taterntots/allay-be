58664bd12bb3805141a3fe10e05087f6c1dbf843
spencer-mcguire <smcguire1826@gmail.com>
2020-02-24 12:29:47 -0600

    addUser test

--- helpers/users-model.spec.js
+++ helpers/users-model.spec.js
@@ -1,7 +1,9 @@
 const request = require('supertest');
 const server = require('../api/server');
 const db = require('../data/dbConfig');
+const Users = require('./users-model');
 
+//sanity check
 describe('server', () => {
 	beforeEach(async () => {
 		await db('users').truncate();
@@ -14,6 +16,23 @@ describe('server', () => {
 	describe('test env', () => {
 		it('should run the testing env', () => {
 			expect(process.env.DB_ENV).toBe('testing');
+		});
+	});
+});
+
+// test Users DB
+describe('Users Model', () => {
+	// register or add a new user
+	describe('addUser()', () => {
+		it('should register/add a new user', async () => {
+			await Users.addUser({
+				email: 'test@gmail.com',
+				username: 'test',
+				password: 'test123'
+			});
+
+			const users = await db('users');
+			expect(users).toHaveLength(1);
 		});
 	});
 });
