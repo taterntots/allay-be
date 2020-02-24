8abd1de8d6ac0529202b2664d7dcffec4fba07c4
spencer-mcguire <smcguire1826@gmail.com>
2020-02-24 12:42:31 -0600

    update addUser() test

--- helpers/users-model.spec.js
+++ helpers/users-model.spec.js
@@ -24,15 +24,33 @@ describe('server', () => {
 describe('Users Model', () => {
 	// register or add a new user
 	describe('addUser()', () => {
-		it('should register/add a new user', async () => {
-			await Users.addUser({
-				email: 'test@gmail.com',
-				username: 'test',
-				password: 'test123'
-			});
+		it('can add a user to the DB', async () => {
+			//mock users
+			const mock_user_1 = {
+				email: 'test1@gmail.com',
+				username: 'IronMan',
+				password: 'IAmIronMan'
+			};
+			const mock_user_2 = {
+				email: 'test2@gmail.com',
+				username: 'Thor',
+				password: 'GodOfThunder'
+			};
+			const mock_user_3 = {
+				email: 'test3@gmail.com',
+				username: 'Rocket',
+				password: 'M3chanic'
+			};
+
+			await Users.addUser(mock_user_1);
+			await Users.addUser(mock_user_2);
+			await Users.addUser(mock_user_3);
 
 			const users = await db('users');
-			expect(users).toHaveLength(1);
+			// check they have been added to the db
+			expect(users).toHaveLength(3);
+			// double check db for a specific username
+			expect(users[1].username).toBe(mock_user_2.username);
 		});
 	});
 });
