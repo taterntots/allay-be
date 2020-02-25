57cb975f8568195545804feb637ff1342d97e4c9
spencer-mcguire <smcguire1826@gmail.com>
2020-02-25 11:03:53 -0600

    expect body.id to be not null

--- routers/companies-router.js
+++ routers/companies-router.js
@@ -11,104 +11,106 @@ const Rev = require('../helpers/reviews-model.js');
 
 //************** GET ALL COMPANIES ****************//
 router.get('/', (req, res) => {
-  Co.findCompanies()
-    .then(company => {
-      res.json(company);
-    })
-    .catch(err => res.send(err));
+	Co.findCompanies()
+		.then(company => {
+			res.json(company);
+		})
+		.catch(err => res.send(err));
 });
 
 //*************** GET COMPANIES BY FILTER *****************//
 router.get('/filter', (req, res) => {
-  console.log(req.params, 'req.params ln 23');
-  const filter = req.params.filter;
+	console.log(req.params, 'req.params ln 23');
+	const filter = req.params.filter;
 
-  Co.findCompaniesBy(filter)
-    .then(company => {
-      res.json(company);
-    })
-    .catch(err => res.send(err));
+	Co.findCompaniesBy(filter)
+		.then(company => {
+			res.json(company);
+		})
+		.catch(err => res.send(err));
 });
 
 //*************** GET COMPANY BY ID *****************//
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  Co.findCompanyBy(id)
-    .then(company => {
-      res.json(company);
-    })
-    .catch(err => res.send(err));
+	Co.findCompanyById(id)
+		.then(company => {
+			res.json(company);
+		})
+		.catch(err => res.send(err));
 });
 
 //****** GET REVIEWS ASSOCIATED WITH COMPANY NAME ******//
+
+// refactor using findCompanyReviews()
 router.get('/:id/reviews', (req, res) => {
-  const id = req.params.orgId;
+	const id = req.params.orgId;
 
-  Co.findCompanyBy(id)
-    .then(company => {
-      res.json(company);
-      const filter = req.params.filter;
+	Co.findCompanyById(id)
+		.then(company => {
+			res.json(company);
+			const filter = req.params.filter;
 
-      Rev.findReviewsBy(filter)
-        .then(company => {
-          res.json(company);
-        })
-        .catch(err => res.send(err));
-    })
-    .catch(err => res.send(err));
+			Rev.findReviewsBy(filter)
+				.then(company => {
+					res.json(company);
+				})
+				.catch(err => res.send(err));
+		})
+		.catch(err => res.send(err));
 });
 
 //***************** ADD NEW COMPANY *******************//
 router.post('/', (req, res) => {
-  let company = req.body;
+	let company = req.body;
 
-  Co.addCompany(company)
-    .then(newCompany => {
-      res.status(201).json(newCompany);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: 'There was an error' });
-    });
+	Co.addCompany(company)
+		.then(newCompany => {
+			res.status(201).json(newCompany);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'There was an error' });
+		});
 });
 
 //************* UPDATE COMPANY INFO ****************//
 router.put('/', (req, res) => {
-  const changes = req.body;
-  const id = req.company.id;
+	const changes = req.body;
+	const id = req.company.id;
 
-  Co.updateCompany(id, changes)
-    .then(info => {
-      if (info) {
-        res.status(200).json({ info: changes });
-      } else {
-        res.status(404).json({ message: 'Error locating company info' });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ message: 'Error updating company info' });
-    });
+	Co.updateCompany(id, changes)
+		.then(info => {
+			if (info) {
+				res.status(200).json({ info: changes });
+			} else {
+				res.status(404).json({ message: 'Error locating company info' });
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ message: 'Error updating company info' });
+		});
 });
 
 //****************** DELETE COMPANY ********************//
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const user = await Co.findCompanyById(id);
-    if (user) {
-      const deleted = await Co.deleteCompany(id);
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ message: 'Error locating company.' });
-    }
-  } catch {
-    res
-      .status(500)
-      .json({ message: 'There was an error deleting company account.' });
-  }
+	try {
+		const user = await Co.findCompanyById(id);
+		if (user) {
+			const deleted = await Co.deleteCompany(id);
+			res.status(200).json(user);
+		} else {
+			res.status(404).json({ message: 'Error locating company.' });
+		}
+	} catch {
+		res
+			.status(500)
+			.json({ message: 'There was an error deleting company account.' });
+	}
 });
 
 /**************************************************************************/
