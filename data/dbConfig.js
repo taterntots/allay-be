43d0576a8a31379663b2d5e86d8da29eef1a0de6
spencer-mcguire <smcguire1826@gmail.com>
2020-02-25 21:44:01 -0600

    update db conf for pg

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,16 +1,7 @@
 // for configuring knex and our database
-// require('dotenv').config();
-
-// const knex = require('knex');
-// const config = require('../knexfile');
-
-// const dbEnv = process.env.DB_ENV || 'development';
-
-// module.exports = knex(config[dbEnv]);
 
 const knex = require('knex');
 const knexfile = require('../knexfile');
-// to test locally, change node to DB_ENV
 const env = process.env.NODE_ENV || 'development';
 const configOptions = knexfile[env];
 
