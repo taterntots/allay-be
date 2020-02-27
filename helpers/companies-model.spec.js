3b5f4c155f850cb628ec2a880a24f9819dc8b451
spencer-mcguire <smcguire1826@gmail.com>
2020-02-27 11:09:37 -0600

    updateComany() test

--- helpers/companies-model.spec.js
+++ helpers/companies-model.spec.js
@@ -124,4 +124,43 @@ describe('Companies Model', () => {
 			expect(companies[0].name).toBe(new_company.name);
 		});
 	});
+	describe('updateCompany()', () => {
+		it('can add a an existing company', async () => {
+			// POST new company
+			const new_company = {
+				name: 'Ignacio Test Company',
+				hq_state: 'California',
+				hq_city: 'San Francisco'
+			};
+			const new_company_2 = {
+				name: 'Spencer Test Company',
+				hq_state: 'California',
+				hq_city: 'San Francisco'
+			};
+			const new_company_3 = {
+				name: 'Matt Test Company',
+				hq_state: 'California',
+				hq_city: 'San Francisco'
+			};
+			const update_company = {
+				name: 'Aaron Test Company',
+				hq_state: 'California',
+				hq_city: 'San Francisco'
+			};
+
+			// add the companies to the database
+			await Company.addCompany(new_company);
+			await Company.addCompany(new_company_2);
+			await Company.addCompany(new_company_3);
+
+			//update the company
+			await Company.updateCompany(2, update_company);
+
+			//access db
+			const companies = await db('companies');
+			// tests
+			expect(companies).toHaveLength(3);
+			expect(companies[2].name).toBe(update_company.name);
+		});
+	});
 });
