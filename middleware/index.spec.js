d63406c1ecbe94a654aa8b98aba40df956503657
taterntots <matthew.a.weidner@gmail.com>
2020-02-27 17:27:33 -0500

    wrote three tests for restriction middleware

---
+++ middleware/index.spec.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const server = require('../api/server');
+const Users = require('../helpers/users-model');
+const db = require('../data/dbConfig');
+
+describe('server.js', () => {
+  beforeEach(async () => {
+    await db.raw('truncate table users cascade');
+    await db.raw('truncate table reviews cascade');
+    await db.raw('truncate table companies cascade'); //clears table for testing purposes before each test
+  });
+
+  /*************************** RESTRICTED *******************************/
+
+  describe('Restriction Middleware', () => {
+    it('should deny a user access to reviews dashboard with missing token', async () => {
+      // get a list of reviews
+      res = await request(server)
+        .get('/api/reviews')
+      //should deny access without token/login
+      expect(res.status).toEqual(401);
+    });
+    it('should deny a user access to companies dashboard with missing token', async () => {
+      // get a list of companies
+      res = await request(server)
+        .get('/api/companies')
+      //should deny access without token/login
+      expect(res.status).toEqual(401);
+    });
+    it('should deny a user access to users dashboard with missing token', async () => {
+      // get a list of companies
+      res = await request(server)
+        .get('/api/users')
+      //should deny access without token/login
+      expect(res.status).toEqual(401);
+    });
+  });
+  /*************************** CHECK FOR REGISTER DATA *******************************/
+
+});
