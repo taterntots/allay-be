473cafa28d0d77a63b977c081273409bb64e7ac0
taterntots <matthew.a.weidner@gmail.com>
2020-02-28 17:38:26 -0500

    messing around with the companies seed file. added a csv file to the data folder

--- data/seeds/02-companies.js
+++ data/seeds/02-companies.js
@@ -1,23 +1,39 @@
-const faker = require('faker');
+const seeder = require('knex-csv-seeder');
 
-const createFakeCompany = () => ({
-  name: faker.company.companyName(),
-  hq_city: faker.address.city(),
-  hq_state: faker.address.state()
-});
+exports.seed = seeder({
+  table: 'companies',
+  file: '../list-of-companies.csv'
+})
 
-exports.seed = function(knex) {
-  // Creates 50 fake companies
-  const fakeCompanies = [];
-  const desiredFakeCompanies = 50;
-  for (let i = 0; i < desiredFakeCompanies; i++) {
-    fakeCompanies.push(createFakeCompany());
-  }
-  // Deletes ALL existing entries
-  return knex('companies')
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex('companies').insert(fakeCompanies);
-    });
-};
+// exports.seed = function (knex) {
+//   // Deletes ALL existing entries
+//   return knex('users').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('users').insert()
+//     })
+// }
+
+// const faker = require('faker');
+
+// const createFakeCompany = () => ({
+//   name: faker.company.companyName(),
+//   hq_city: faker.address.city(),
+//   hq_state: faker.address.state()
+// });
+
+// exports.seed = function(knex) {
+//   // Creates 50 fake companies
+//   const fakeCompanies = [];
+//   const desiredFakeCompanies = 50;
+//   for (let i = 0; i < desiredFakeCompanies; i++) {
+//     fakeCompanies.push(createFakeCompany());
+//   }
+//   // Deletes ALL existing entries
+//   return knex('companies')
+//     .del()
+//     .then(function() {
+//       // Inserts seed entries
+//       return knex('companies').insert(fakeCompanies);
+//     });
+// };
