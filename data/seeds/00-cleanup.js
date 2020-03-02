14add3d46201f71ab66eb5bcf080bcea2841b4f0
taterntots <matthew.a.weidner@gmail.com>
2020-03-02 14:17:27 -0500

    added truncating to the knex cleaner file

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -2,6 +2,8 @@ const knexCleaner = require("knex-cleaner");
 
 exports.seed = function(knex) {
   return knexCleaner.clean(knex, {
+    mode: 'truncate',
+    restartIdentity: true,
     ignoreTables: ["knex_migrations", "users", "reviews", "knex_migrations_lock"]
   });
 }
